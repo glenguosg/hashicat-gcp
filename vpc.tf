d6fa91f67ba225007e7c86421dd78463681e26f9
glenguosg <guo.yiming@outlook.com>
2021-11-05 13:29:20 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,16 +1,18 @@
-module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 3.4.0"
+module "network" {
+  source  = "app.terraform.io/hashicorp-myorg/network/google"
+  version = "3.4.0"
 
-    project_id   = var.project
-    network_name = "myorg-vpc"
-    routing_mode = "GLOBAL"
+  project_id   = var.project
+  network_name = "myorg-vpc"
+  routing_mode = "GLOBAL"
 
-    subnets = [
-        {
-            subnet_name           = "myorg-subnet"
-            subnet_ip             = "10.10.10.0/24"
-            subnet_region         = var.region
-        }
-    ]
+  subnets = [
+    {
+      subnet_name   = "myorg-subnet"
+      subnet_ip     = "10.10.10.0/24"
+      subnet_region = var.region
+    }
+  ]
+
+
 }
